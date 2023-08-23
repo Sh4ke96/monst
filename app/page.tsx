1998827314ab1c3e90a2ca4b41e2fe025af00817
Artur Michałek <sh4ke96@gmail.com>
2023-08-23 19:34:46 +0200

    start to create section contact - info + icons

--- app/page.tsx
+++ app/page.tsx
@@ -446,6 +446,81 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section>
+        <div className="container my-16">
+          <div className="text-center">
+            <h3 className="text-black font-bold text-3xl">Get in touch!</h3>
+            <p className="text-gray/50 text-base font-light">
+              We will be glad to hear from you
+            </p>
+          </div>
+          <div className="flex justify-center my-8 gap-x-16">
+            <div className="flex flex-col items-center justify-center">
+              <svg
+                className="mb-6 h-8 w-8 mx-auto text-blue-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="#3B82F6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                ></path>
+              </svg>
+              <p className="text-gray/50 font-light text-base mb-3">Phone</p>
+              <p className="text-black font-light">+ 48 654-430-309</p>
+              <p className="text-black font-light">+ 1 6532-430-309</p>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <svg
+                className="mb-6 h-8 w-8 mx-auto text-blue-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="#3B82F6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                ></path>
+              </svg>
+              <p className="text-gray/50 font-light text-base mb-3">E-mail</p>
+              <p className="text-black font-light">contact@monst.com</p>
+              <p className="text-black font-light">pat@example.com</p>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <svg
+                className="mb-6 h-8 w-8 mx-auto text-blue-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="#3B82F6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                ></path>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                ></path>
+              </svg>
+              <p className="text-gray/50 font-light text-base mb-3">Address</p>
+              <p className="text-black font-light">+ 48 654-430-309</p>
+              <p className="text-black font-light">+ 1 6532-430-309</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
